e893fd3cf66f542e87e66d169a0809b70f9bef2b
RecycledFenix <tomaserobino@gmail.com>
2022-11-10 17:59:06 -0300

    Highlighting of product on hover, change on click

--- src/Central/ProductPanel.js
+++ src/Central/ProductPanel.js
@@ -6,29 +6,18 @@ function ProductPanel(props) {
     for(let i in productDatabase) { //Loops through all products
         if (props.category==="all" || props.category===productDatabase[i].category) {  //Check for category filter
             if (productDatabase[i].price >= props.priceFilter[0] && productDatabase[i].price <= props.priceFilter[1]) { //Check for price filter
-                
-                //dataArr[0].push(productDatabase[i].productName)
-                
-                divArr.push(<div className="div">
-                    {productDatabase[i].productName}
-                    <img src={productDatabase[i].imgs.main} className="image"/>
+                divArr.push(<div className="productDiv">
+                    <div className="productImageDiv">
+                        <img src={productDatabase[i].imgs.main} className="image"/>
+                    </div>
+                    <div>
+                        <h3>{productDatabase[i].productName}</h3>
+                        {productDatabase[i].price}
+                    </div>
                 </div>);
-
-
-                //displayedArr.push(productDatabase[i]);
-                
             }
         }
     }
-/*
-    for(let i in imgGroupArr) {
-        divArr.push(<div className="div">
-            {dataArr[0][i]}
-            <img src={imgGroupArr[i].main} className="image"></img>
-        </div>);
-    }
-*/
-
 
     return (
         <div id="productPanel">
