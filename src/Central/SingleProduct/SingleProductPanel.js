8bdedda487b7e33713571fa39b2cef1d88082c36
tomasrobino <tomaserobino@gmail.com>
2023-01-05 21:52:43 +0100

    added image viewer

--- src/Central/SingleProduct/SingleProductPanel.js
+++ src/Central/SingleProduct/SingleProductPanel.js
@@ -1,9 +1,35 @@
+import { useState } from "react";
+
 function SingleProductPanel(props) {
+	const [source, setSource] = useState(props.currentProd.imgs.main);
+	const [count, setCount] = useState(1);
+
+	let imgs = [];
+	
+
+	for (let i in props.currentProd.imgs) {
+		imgs.push(i);
+	}
+
+	function handleClick() {
+		if (count === imgs.length) {
+			setCount(1);
+			setSource(props.currentProd.imgs.main);
+		} else {
+			let aux = count+1;
+			setCount(aux);
+			setSource(props.currentProd.imgs[imgs[count]]);
+			console.log(imgs[count]);
+		}
+	}
+
 	return (
 		<div className="singleProductPanel">
-		    <img className="productImage" src={props.currentProd.imgs.main}/>
+			<div className="viewerDiv">
+				<img className="productImage" src={source} onClick={handleClick}/>
+			</div>
 		</div>
-		);
+	);
 }
 
 export default SingleProductPanel;
